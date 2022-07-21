79bcc8192727306eb746628647189cac378800f8
yanun <yanunyang@psp-power.com.tw>
2022-07-22 01:24:58 +0800

    add color

--- Sources/UIComponent/Extension/Color.swift
+++ Sources/UIComponent/Extension/Color.swift
@@ -13,16 +13,36 @@ extension Color {
     public static let primary75: Self = .primary.opacity(0.75)
     public static let primary50: Self = .primary.opacity(0.5)
     public static let primary25: Self = .primary.opacity(0.25)
-    public static var background: Self { Dynamic(.red, .blue) }
     public static func Dynamic(_ lightMode: Self, _ darkMode: Self) -> Self {
-        @Environment(\.colorScheme) var colorScheme: ColorScheme
+        DynamicColor(lightMode, darkMode).Adapter.DisplayColor
+    }
+}
+
+@available(iOS 15, macOS 12.0, *)
+public class DynamicColor: ObservableObject {
+    @Published public var Adapter: DynamicColorAdapter
+    public init(_ lightMode: Color, _ darkMode: Color) {
+        self.Adapter = DynamicColorAdapter(lightMode, darkMode)
+    }
+}
+
+@available(iOS 15, macOS 12.0, *)
+public struct DynamicColorAdapter {
+    @Environment(\.colorScheme) private var colorScheme: ColorScheme
+    private var lightModeColor: Color
+    private var darkModeColor: Color
+    public init(_ light: Color, _ dark: Color){
+        self.lightModeColor = light
+        self.darkModeColor = dark
+    }
+    public var DisplayColor: Color {
         switch colorScheme {
         case .light:
-            return lightMode
+            return lightModeColor
         case .dark:
-            return darkMode
+            return darkModeColor
         @unknown default:
-            return lightMode
+            return lightModeColor
         }
     }
 }

c0a94878fe66f4c4c30b6c5430f713d1a88f7936
yanun <yanunyang@psp-power.com.tw>
2022-07-21 23:10:52 +0800

    add color

--- Sources/UIComponent/Extension/Color.swift
+++ Sources/UIComponent/Extension/Color.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 @available(iOS 15, macOS 12.0, *)
 extension Color {
-    static let extend = ColorCustom()
+    public static let extend = ColorCustom()
 }
 
 @available(iOS 15, macOS 12.0, *)
-private struct ColorCustom {
+public struct ColorCustom {
     let primary = Color.primary
     let secondary = Color.primary.opacity(0.75)
     let gray = Color.primary.opacity(0.5)
     let fade = Color.primary.opacity(0.25)
-    let background = Color.background
 }

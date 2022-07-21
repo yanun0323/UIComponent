622bb5ad708dd7a5b65502ae79ad145208894743
yanun <yanunyang@psp-power.com.tw>
2022-07-21 23:03:28 +0800

    add color

---
+++ Sources/UIComponent/Extension/Color.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by YanunYang on 2022/7/21.
+//
+
+import Foundation
+
+extension Color {
+    static let extend = ColorCustom
+}
+
+private struct ColorCustom {
+    let primary = Color.primary
+    let secondary = Color.primary.opacity(0.75)
+    let gray = Color.primary.opacity(0.5)
+    let fade = Color.primary.opacity(0.25)
+    let background = Color.background
+}

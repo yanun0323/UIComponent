2d9fba605bdc9a52048cf211455496050f0c0da0
yanun <yanunyang@psp-power.com.tw>
2022-08-14 20:38:16 +0800

    add DateOffset

---
+++ Sources/UIComponent/Model/DateOffset.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by YanunYang on 2022/8/14.
+//
+
+
+@available(iOS 15, macOS 12.0, *)
+public enum DateOffset: Hashable {
+    case Day, Week, Month, Year
+    
+    public func Date(_ date: Date) -> Date {
+        switch self {
+        case .Day:
+            return date.Next()
+        case .Week:
+            return date.AddWeek(week: 1)
+        case .Month:
+            return date.AddMonth(month: 1)
+        case .Year:
+            return date.AddYear(year: 1)
+        }
+    }
+}

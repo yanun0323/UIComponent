5ee9cbb5ffd0e603a48d804ab770173bb78db662
yanun <yanunyang@psp-power.com.tw>
2022-08-14 21:23:55 +0800

    make DateSpan Identifiable

--- Sources/UIComponent/Model/DateSpan.swift
+++ Sources/UIComponent/Model/DateSpan.swift
@@ -8,5 +8,21 @@
 
 @available(iOS 15, macOS 12.0, *)
 public enum DateSpan: Identifiable, Hashable, CaseIterable, Codable {
+    var ID: Int { self.Int() }
     case None, Day, Week, Month, Year
+    
+    public func Int() -> Int {
+        switch self {
+        case .None:
+            return 0
+        case .Day:
+            return 1
+        case .Week:
+            return 2
+        case .Month:
+            return 3
+        case .Year:
+            return 4
+        }
+    }
 }

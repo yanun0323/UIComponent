23978aebc9641cf032a85224de79bc46987766d4
yanun <yanunyang@psp-power.com.tw>
2022-08-28 15:39:21 +0800

    add device size

---
+++ Sources/UIComponent/Model/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by YanunYang on 2022/8/28.
+//
+
+import Foundation
+
+#if os(iOS)
+import UIKit
+@available(iOS 15, *)
+public class Device {
+    public static var size: CGSize = UIScreen.main.bounds.size
+}
+#endif

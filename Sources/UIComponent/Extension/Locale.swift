5248c68461bc69e6e4543fdbfd4727ba8018dad5
yanun0323 <a1256212562@gmail.com>
2022-10-01 22:20:48 +0800

    fix duplicate public keyword

--- Sources/UIComponent/Extension/Locale.swift
+++ Sources/UIComponent/Extension/Locale.swift
@@ -9,7 +9,7 @@ import Foundation
 
 @available(iOS 15, macOS 12.0, *)
 extension Locale {
-    public static public var TW: Locale = .init(identifier: "zh_tw")
-    public static public var US: Locale = .init(identifier: "en_us")
-    public static public var JP: Locale = .init(identifier: "ja_jp")
+    static public var TW: Locale = .init(identifier: "zh_tw")
+    static public var US: Locale = .init(identifier: "en_us")
+    static public var JP: Locale = .init(identifier: "ja_jp")
 }

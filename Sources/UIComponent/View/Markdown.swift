a06ca413aca67ba5baed21c3686cd2c97151328f
yanun <yanunyang@psp-power.com.tw>
2022-10-11 14:11:58 +0800

    add markdown view init func

--- Sources/UIComponent/View/Markdown.swift
+++ Sources/UIComponent/View/Markdown.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 public struct Markdown: View {
     @Binding var text: String
+    
+    public init(_ text: Binding<String>) {
+        self._text = text
+    }
+    
     public var body: some View {
         MarkdownComponent(text: text) { elem in
             ElementView(element: elem)

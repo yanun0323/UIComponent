87fd0dd15137c941869d413c17052a2ec86d83bd
yanun <yanunyang@psp-power.com.tw>
2022-10-17 00:51:05 +0800

    add parameter to block view

--- Sources/UIComponent/View/Block.swift
+++ Sources/UIComponent/View/Block.swift
@@ -4,15 +4,17 @@ import SwiftUI
 public struct Block: View {
     var width: CGFloat?
     var height: CGFloat?
+    var color: Color
     
-    public init(width: CGFloat? = nil, height: CGFloat? = nil) {
+    public init(width: CGFloat? = nil, height: CGFloat? = nil, color: Color = .clear) {
         self.width = width
         self.height = height
+        self.color = color
     }
     
     public var body: some View {
         Rectangle()
-            .foregroundColor(.clear)
+            .foregroundColor(color)
             .frame(width: width, height: height)
     }
 }

41bca70de5a9abbe5ea9f7d9036c747419d43c37
yanun <yanunyang@psp-power.com.tw>
2022-10-27 01:35:32 +0800

    add public init to Blank View

--- Sources/UIComponent/View/BlankView.swift
+++ Sources/UIComponent/View/BlankView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @available(iOS 15, macOS 12.0, *)
 public struct BlankView: View {
+    
+    public init() {}
+    
     public var body: some View {
         Rectangle()
             .foregroundColor(.transparent)

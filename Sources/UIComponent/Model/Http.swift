5464e27579fe8d747713c21c9641cda808362119
yanun <yanunyang@psp-power.com.tw>
2022-11-01 07:28:07 +0800

    update Http struct

--- Sources/UIComponent/Model/Http.swift
+++ Sources/UIComponent/Model/Http.swift
@@ -42,4 +42,59 @@ extension Http {
         channel.wait()
         return result
     }
+    
+    public func DumpRequest(toUrl path: String, action: @escaping (URLRequest) -> URLRequest) -> String {
+        guard let url = URL(string: path) else {
+            print("failed to generate url from string: \(path)")
+            return ""
+        }
+        let channel = DispatchSemaphore(value: 0)
+        var result: String = ""
+        
+        var request = URLRequest(url: url, timeoutInterval: 30)
+        request = action(request)
+        
+        URLSession.shared.dataTask(with: request) { data, responce, error in
+            defer { channel.signal() }
+            guard
+                let data = data,
+                error == nil ,
+                let responce = responce as? HTTPURLResponse,
+                responce.statusCode == 200 else {
+                print("Failed to download data")
+                return
+            }
+            print("complete download, data length: \(data.count)")
+            result = String(decoding: data, as: UTF8.self)
+            
+        }.resume()
+        channel.wait()
+        return result
+    }
+    
+    public func RequestStatusCode(toUrl path: String, action: @escaping (URLRequest) -> URLRequest) -> Int? {
+        guard let url = URL(string: path) else {
+            print("failed to generate url from string: \(path)")
+            return nil
+        }
+        let channel = DispatchSemaphore(value: 0)
+        var result: Int? = nil
+        
+        var request = URLRequest(url: url, timeoutInterval: 30)
+        request = action(request)
+        
+        URLSession.shared.dataTask(with: request) { _, responce, error in
+            defer { channel.signal() }
+            guard
+                error == nil ,
+                let responce = responce as? HTTPURLResponse else {
+                print("Failed to download data")
+                return
+            }
+            result = responce.statusCode
+            
+        }.resume()
+        channel.wait()
+        return result
+    }
 }

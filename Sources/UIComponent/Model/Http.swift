67be1f5413ff3cd2535859622537e06d674b84d3
yanun <yanunyang@psp-power.com.tw>
2022-11-01 07:17:18 +0800

    add Http struct

---
+++ Sources/UIComponent/Model/Http.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by YanunYang on 2022/11/1.
+//
+
+import Foundation
+
+struct Http {
+    
+}
+
+extension Http {
+    static func SendRequest<T>(toUrl path: String, type: T.Type, action: @escaping (URLRequest) -> URLRequest) -> T? where T: Decodable {
+        guard let url = URL(string: path) else {
+            print("failed to generate url from string: \(path)")
+            return nil
+        }
+        let channel = DispatchSemaphore(value: 0)
+        var result: T? = nil
+        
+        var request = URLRequest(url: url, timeoutInterval: 30)
+        request = action(request)
+        
+        URLSession.shared.dataTask(with: request) { data, responce, error in
+            defer { channel.signal() }
+            guard
+                let data = data,
+                error == nil ,
+                let responce = responce as? HTTPURLResponse,
+                responce.statusCode == 200 else {
+                print("Failed to download data")
+                return
+            }
+            print("complete download, data length: \(data.count)")
+            guard let decoded = try? JSONDecoder().decode(T.self, from: data) else {
+                print("decode data failed")
+                return
+            }
+            result = decoded
+            
+        }.resume()
+        channel.wait()
+        return result
+    }
+}

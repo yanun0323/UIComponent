9aac1d6749bb61db9942206ed0b103cb4b0b26ea
yanun <yanunyang@psp-power.com.tw>
2022-11-01 10:07:39 +0800

    update the documentation of CacheTimer

--- Sources/UIComponent/Model/CacheTImer.swift
+++ Sources/UIComponent/Model/CacheTImer.swift
@@ -9,6 +9,23 @@ public class CacheTimer {
   private let defaultCountdown: Int
   private var action: () -> Void
 
+    /**
+     Create a count down timer that execute the action when countdown reach to zero
+     
+     - Action will be execute when `Countdown/(1/TimeInterval)` **seconds** passed
+     - Call **Activate()** after create timer to activate the timer
+     
+     - Parameters:
+        - countdown:
+                    Reduce 1 every timeInterval passed.
+                    when countdown reduce to 0, `action` function invokes immediately.
+        - timeInterval:
+                    The number of seconds. every timeInterval pass, countdown reduce 1.
+        - action:
+                    A block to be executed when the countdwon reduce to 0.
+     
+     
+     */
   public init(countdown: Int, timeInterval: Double, action: @escaping () -> Void) {
     self.timer = nil
     self.countdown = 0
@@ -21,7 +38,13 @@ public class CacheTimer {
 
 @available(iOS 15, macOS 12.0, *)
 extension CacheTimer {
-  public func Init() {
+    /**
+     Activate the timer
+     
+     - This function **WILL NOT** start counting down
+     - Call **Refresh()** after **Activate()** to start/refresh counting down
+     */
+  public func Activate() {
     if timer != nil {
       return
     }
@@ -41,11 +64,17 @@ extension CacheTimer {
       })
   }
 
+    /**
+     Reset the countdown number and keep counting down
+     */
   public func Refresh() {
     self.excutable = true
     self.countdown = self.defaultCountdown
   }
 
+    /**
+     Set countdown number to 0 without execute the action
+     */
   public func SkipAction() {
     self.excutable = false
     self.countdown = 0

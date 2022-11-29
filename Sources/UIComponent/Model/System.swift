ea384978eb8d7cd7ed71131b72ce58b7aee318ba
yanun <yanunyang@psp-power.com.tw>
2022-11-29 17:44:44 +0800

    add System.build

--- Sources/UIComponent/Model/System.swift
+++ Sources/UIComponent/Model/System.swift
@@ -3,7 +3,8 @@ import AppKit
 
 @available(macOS 12.0, iOS 15, *)
 public struct System {
-    public static let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+    public static let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "-"
+    public static let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "-"
 }
 
 @available(macOS 12.0, iOS 15, *)

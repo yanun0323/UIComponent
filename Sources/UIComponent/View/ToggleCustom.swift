eaf3f71d0c16da63f02e9299c6dc96eae39280d1
yanun <yanunyang@psp-power.com.tw>
2022-12-28 13:42:12 +0800

    fix toggle param

--- Sources/UIComponent/View/ToggleCustom.swift
+++ Sources/UIComponent/View/ToggleCustom.swift
@@ -4,15 +4,22 @@ import SwiftUI
 public struct ToggleCustom: View {
     @Binding var isOn: Bool
     @Binding var color: Color
-    var size: CGFloat = 26
-    var ratio: CGFloat = 1.7
-    let gap: CGFloat = 2
+    var size: CGFloat
+    var ratio: CGFloat
+    var gap: CGFloat
     
-    init(isOn: Binding<Bool>, color: Binding<Color>, size: CGFloat, ratio: CGFloat) {
+    init(
+        isOn: Binding<Bool>,
+        color: Binding<Color>,
+        size: CGFloat = 26,
+        ratio: CGFloat = 1.7,
+        gap: CGFloat = 2
+    ) {
         self._isOn = .init(projectedValue: isOn)
         self._color = .init(projectedValue: color)
         self.size = size
         self.ratio = ratio
+        self.gap = gap
     }
     
     public var body: some View {

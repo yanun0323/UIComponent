55ba8575aad5cd8f109bf0e86c869d53b8bbc519
yanun0323 <a1256212562@gmail.com>
2023-03-09 04:25:03 +0800

    update timezone property

--- Sources/UIComponent/Extension/Timezone.swift
+++ Sources/UIComponent/Extension/Timezone.swift
@@ -2,5 +2,5 @@ import SwiftUI
 
 @available(iOS 15, macOS 12.0, *)
 extension TimeZone {
-    static let UTC = NSTimeZone(name: "UTC")! as TimeZone
+    public static let UTC = NSTimeZone(name: "UTC")! as TimeZone
 }

482101f4a842c0944ba493d3508e1e2dff8ee3cc
yanun <yanunyang@psp-power.com.tw>
2023-04-12 21:30:00 +0800

    add style param into half view

--- Sources/UIComponent/View/HalfSheet.swift
+++ Sources/UIComponent/View/HalfSheet.swift
@@ -1,50 +1,74 @@
 import SwiftUI
+
 #if os(iOS)
-// MARK: Controller
-@available(iOS 15, *)
-public class HalfSheetController<Content>: UIHostingController<Content> where Content : View {
-    public var grabber: Bool = true
-    public var radius: CGFloat = 15
-    
-    
-    override public func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if let presentation = sheetPresentationController {
-            presentation.detents = [.medium(), .large()]
-            presentation.prefersGrabberVisible = grabber
-            presentation.largestUndimmedDetentIdentifier = .medium
-            presentation.preferredCornerRadius = radius
+    @available(iOS 15, *)
+    public enum HalfSheetStyle {
+        case medium, large
+    }
+
+    // MARK: Controller
+    @available(iOS 15, *)
+    public class HalfSheetController<Content>: UIHostingController<Content> where Content: View {
+        public var grabber: Bool = true
+        public var radius: CGFloat = 15
+        public var detents: [UISheetPresentationController.Detent] = []
+
+        override public func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+
+            if let presentation = sheetPresentationController {
+                presentation.detents = detents  // [.medium(), .large()]
+                presentation.prefersGrabberVisible = grabber
+                presentation.largestUndimmedDetentIdentifier = .medium
+                presentation.preferredCornerRadius = radius
+            }
+        }
+
+        public func Inject(
+            _ grabberVisible: Bool, _ cornerRadius: CGFloat, _ style: [HalfSheetStyle]
+        ) -> Self {
+            self.grabber = grabberVisible
+            self.radius = cornerRadius
+            if let s = style {
+                s.forEach { ss in
+                    switch ss {
+                    case .medium:
+                        self.detents.append(.medium())
+                    case .large:
+                        self.detents.append(.large())
+
+                    }
+                }
+            }
+            return self
         }
     }
-    
-    public func Inject(_ grabberVisible: Bool, _ cornerRadius: CGFloat) -> Self {
-        self.grabber = grabberVisible
-        self.radius = cornerRadius
-        return self
-    }
-}
 
-// MARK: Structure
-@available(iOS 15, *)
-@available(macOS, unavailable)
-public struct HalfSheet<Content>: UIViewControllerRepresentable where Content : View {
-    
-    public let grabber: Bool
-    public let radius: CGFloat
-    public let content: Content
-    
-    public init(grabber: Bool = true, radius: CGFloat = 15, @ViewBuilder content: () -> Content) {
-        self.grabber = grabber
-        self.radius = radius
-        self.content = content()
+    // MARK: Structure
+    @available(iOS 15, *)
+    @available(macOS, unavailable)
+    public struct HalfSheet<Content>: UIViewControllerRepresentable where Content: View {
+
+        public let grabber: Bool
+        public let radius: CGFloat
+        public let style: [HalfSheetStyle]
+        public let content: Content
+
+        public init(
+            grabber: Bool = true, radius: CGFloat = 15, style: [HalfSheetStyle] = [.medium],
+            @ViewBuilder content: () -> Content
+        ) {
+            self.grabber = grabber
+            self.radius = radius
+            self.style = style
+            self.content = content()
+        }
+
+        public func makeUIViewController(context: Context) -> HalfSheetController<Content> {
+            return HalfSheetController(rootView: content).Inject(grabber, radius, style)
+        }
+
+        public func updateUIViewController(_: HalfSheetController<Content>, context: Context) {
+        }
     }
-    
-    public func makeUIViewController(context: Context) -> HalfSheetController<Content> {
-        return HalfSheetController(rootView: content).Inject(grabber, radius)
-    }
-    
-    public func updateUIViewController(_: HalfSheetController<Content>, context: Context) {
-    }
-}
 #endif
